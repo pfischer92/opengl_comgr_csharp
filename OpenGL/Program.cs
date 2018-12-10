41265dfe5c05b32b76918837c37ac49da970738c
Patrick Fischer <patrick.fischer.privat@gmail.com>
2018-12-10 10:34:27 +0100

    init

---
+++ OpenGL/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenTK;                  //add "OpenTK" as NuGet reference
+using OpenTK.Graphics.OpenGL4; //add "OpenTK" as NuGet reference
+
+namespace OpenGL
+{
+    static class Program
+    {
+        static void Main()
+        {
+            using (var w = new GameWindow(720, 480, null, "ComGr", GameWindowFlags.Default, DisplayDevice.Default, 4, 0, OpenTK.Graphics.GraphicsContextFlags.ForwardCompatible))
+            {
+                int hProgram = 0;
+
+                int vaoTriangle = 0;
+                int[] triangleIndices = null;
+                int vboTriangleIndices = 0;
+
+                double time = 0;
+
+                w.Load += (o, ea) =>
+                {
+                    //set up opengl
+                    GL.Enable(EnableCap.FramebufferSrgb);
+                    GL.ClearColor(0.5f, 0.5f, 0.5f, 0);
+                    //GL.ClearDepth(1);
+                    //GL.Disable(EnableCap.DepthTest);
+                    //GL.DepthFunc(DepthFunction.Less);
+                    //GL.Disable(EnableCap.CullFace);
+
+                    //load, compile and link shaders
+                    //see https://www.khronos.org/opengl/wiki/Vertex_Shader
+                    var VertexShaderSource = @"
+                        #version 400 core
+
+                        in vec3 pos;
+                        uniform float time;
+                        out float someFloat;
+
+                        void main()
+                        {
+                          gl_Position = vec4(pos, 1.0) + vec4(sin(time) * 0.5, cos(time) * 0.5, 0.0, 0.0);
+                          someFloat = pos.x + 0.5;
+                        }
+                        ";
+                    var hVertexShader = GL.CreateShader(ShaderType.VertexShader);
+                    GL.ShaderSource(hVertexShader, VertexShaderSource);
+                    GL.CompileShader(hVertexShader);
+                    GL.GetShader(hVertexShader, ShaderParameter.CompileStatus, out int status);
+                    if (status != 1)
+                        throw new Exception(GL.GetShaderInfoLog(hVertexShader));
+
+                    //see https://www.khronos.org/opengl/wiki/Fragment_Shader
+                    var FragmentShaderSource = @"
+                        #version 400 core
+
+                        out vec4 colour;
+                        in float someFloat;
+
+                        void main()
+                        {
+                          colour = vec4(someFloat, 0.75, 0.0, 1.0);
+                        }
+                        ";
+                    var hFragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+                    GL.ShaderSource(hFragmentShader, FragmentShaderSource);
+                    GL.CompileShader(hFragmentShader);
+                    GL.GetShader(hFragmentShader, ShaderParameter.CompileStatus, out status);
+                    if (status != 1)
+                        throw new Exception(GL.GetShaderInfoLog(hFragmentShader));
+
+                    //link shaders to a program
+                    hProgram = GL.CreateProgram();
+                    GL.AttachShader(hProgram, hFragmentShader);
+                    GL.AttachShader(hProgram, hVertexShader);
+                    GL.LinkProgram(hProgram);
+                    GL.GetProgram(hProgram, GetProgramParameterName.LinkStatus, out status);
+                    if (status != 1)
+                        throw new Exception(GL.GetProgramInfoLog(hProgram));
+
+                    //upload model vertices to a vbo
+                    var triangleVertices = new float[]
+                    {
+                       0.0f, -0.5f, 0.0f,
+                       0.5f,  0.5f, 0.0f,
+                      -0.5f,  0.5f, 0.0f
+                    };
+                    var vboTriangleVertices = GL.GenBuffer();
+                    GL.BindBuffer(BufferTarget.ArrayBuffer, vboTriangleVertices);
+                    GL.BufferData(BufferTarget.ArrayBuffer, triangleVertices.Length * sizeof(float), triangleVertices, BufferUsageHint.StaticDraw);
+
+                    // upload model indices to a vbo
+                    triangleIndices = new int[] { 0, 1, 2 };
+                    vboTriangleIndices = GL.GenBuffer();
+                    GL.BindBuffer(BufferTarget.ElementArrayBuffer, vboTriangleIndices);
+                    GL.BufferData(BufferTarget.ElementArrayBuffer, triangleIndices.Length * sizeof(int), triangleIndices, BufferUsageHint.StaticDraw);
+
+                    //set up a vao
+                    vaoTriangle = GL.GenVertexArray();
+                    GL.BindVertexArray(vaoTriangle);
+                    var posAttribIndex = GL.GetAttribLocation(hProgram, "pos");
+                    if (posAttribIndex != -1)
+                    {
+                        GL.EnableVertexAttribArray(posAttribIndex);
+                        GL.BindBuffer(BufferTarget.ArrayBuffer, vboTriangleVertices);
+                        GL.VertexAttribPointer(posAttribIndex, 3, VertexAttribPointerType.Float, false, 0, 0);
+                    }
+
+                    //check for errors during all previous calls
+                    var error = GL.GetError();
+                    if (error != ErrorCode.NoError)
+                        throw new Exception(error.ToString());
+                };
+
+                w.UpdateFrame += (o, fea) =>
+                {
+                    //perform logic
+
+                    time += fea.Time;
+                };
+
+                w.RenderFrame += (o, fea) =>
+                {
+                    //clear screen and z-buffer
+                    GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+                    //switch to our shader
+                    GL.UseProgram(hProgram);
+                    var timeUniformIndex = GL.GetUniformLocation(hProgram, "time");
+                    if (timeUniformIndex != -1)
+                        GL.Uniform1(timeUniformIndex, (float)time);
+
+                    //model-view-projection matrix (not yet used)
+                    var mvp =
+                        //model
+                        Matrix4.Identity
+
+                        //view
+                        * Matrix4.LookAt(new Vector3(0, 0, -10), new Vector3(0, 0, 0), new Vector3(0, 1, 0)) //view
+
+                        //projection
+                        * Matrix4.CreatePerspectiveFieldOfView(45 * (float)(Math.PI / 180d), w.ClientRectangle.Width / (float)w.ClientRectangle.Height, 0.1f, 100f);
+
+                    //render our model
+                    GL.BindVertexArray(vaoTriangle);
+                    GL.BindBuffer(BufferTarget.ElementArrayBuffer, vboTriangleIndices);
+                    GL.DrawElements(PrimitiveType.Triangles, triangleIndices.Length, DrawElementsType.UnsignedInt, 0);
+
+                    //display
+                    w.SwapBuffers();
+
+                    var error = GL.GetError();
+                    if (error != ErrorCode.NoError)
+                        throw new Exception(error.ToString());
+                };
+
+                w.Resize += (o, ea) =>
+                {
+                    GL.Viewport(w.ClientRectangle);
+                };
+
+                w.Run();
+            }
+        }
+    }
+}
